391bc34c1c68cf75744dc1b410382481f97dd95e
kasim23 <qsudais@gmail.com>
2025-03-09 15:55:45 -0400

    repl implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#[allow(unused_imports)]
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+
+    // Wait for user input
+    let stdin = io::stdin();
+    
+
+    loop{
+        // Print prompt inside the loop so it's shown on every iteration
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        
+        let mut input = String::new();
+
+        if stdin.read_line(&mut input).unwrap() == 0{
+            break;
+        }
+
+        let command = input.trim();
+
+        // For now every command is considered invalid
+        println!("{}: command not found", command);
+        input.clear();
+    }
+
 }
